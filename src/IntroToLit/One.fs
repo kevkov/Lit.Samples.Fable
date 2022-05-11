b5b15594b3f582a4a1ee3c9686af0f1da5804250
kevkovh <22GNewReggy@^>
2022-05-11 18:03:23 +0100

    initial files

---
+++ src/IntroToLit/One.fs
@@ -0,0 +1,16 @@
+﻿module Lit.TodoMVC.IntroToLit.One
+
+open Lit
+
+[<LitElement("my-element")>]
+let MyElement() =
+    // This call is obligatory to initialize the web component
+    let _, props =
+        LitElement.init(fun init ->
+            init.props <- {| version = Prop.Of(defaultValue = 0) |})
+        
+    html
+        $"""
+            <p>Welcome to the Lit tutorial!</p>
+            <p>This is the ${props.version} code.</p>
+        """
---
+++ src/IntroToLit/Two.fs
@@ -0,0 +1,14 @@
+﻿module Lit.TodoMVC.IntroToLit.Two
+
+open Lit
+
+[<LitElement("my-element")>]
+let MyElement() =
+    // This call is obligatory to initialize the web component
+    let _, props =
+        LitElement.init(fun init -> () )
+        
+    html
+        $"""
+            <p>Hello world! From my-element.</p>
+        """
