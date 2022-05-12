b71dbd3e5955a20d91ab40ba67bc53c02c476161
kevkovh <22GNewReggy@^>
2022-05-12 21:39:46 +0100

    intro to lit 4

---
+++ src/IntroToLit/Four.fs
@@ -0,0 +1,21 @@
+﻿module Lit.TodoMVC.IntroToLit.Four
+
+open Browser.Types
+open Fable.Import
+open Lit
+
+[<LitElement("name-tag")>]
+let NameTag() =
+    // This call is obligatory to initialize the web component
+    let _, props =
+        LitElement.init(fun init ->  init.props <- {| Name = Prop.Of(defaultValue = "") |} )
+        
+    let changeName (event: Event) =
+        let input = event.target :?> HTMLInputElement
+        props.Name.Value <- input.value
+        ()
+    html
+        $"""
+          <p>Hello, {props.Name}</p>
+          <input @input={changeName} placeholder="Enter your name">
+        """
