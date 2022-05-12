0918ae60737f5cfeb6d930d11c3618a0d443cbec
kevkovh <22GNewReggy@^>
2022-05-12 18:57:38 +0100

    intro to lit 3

---
+++ src/IntroToLit/Three.fs
@@ -0,0 +1,14 @@
+﻿module Lit.TodoMVC.IntroToLit.Three
+
+open Lit
+
+[<LitElement("my-element")>]
+let MyElement() =
+    // This call is obligatory to initialize the web component
+    let _, props =
+        LitElement.init(fun init ->  init.props <- {| message = Prop.Of(defaultValue = "Hello again.") |} )
+        
+    html
+        $"""
+            <p>{props.message}</p>
+        """
