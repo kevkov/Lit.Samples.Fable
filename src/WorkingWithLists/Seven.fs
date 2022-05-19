c401f41eb24d1b68b3c8d19264283b5292c50cde
kev <kev.hannen@gmail.com>
2022-05-19 16:08:32 +0100

    wip

--- src/WorkingWithLists/Seven.fs
+++ src/WorkingWithLists/Seven.fs
@@ -1,11 +1,18 @@
 ﻿namespace Lit.Samples.Fable.WorkingWithLists
 
 module Seven =
-    
-    open Lit
-    
-    [<LitElement("my-element")>]
-    let MyElement() =
-        LitElement.init() |> ignore
-        
 
+    open Lit
+
+    [<LitElement("my-element")>]
+    let MyElement () =
+        LitElement.init () |> ignore
+
+        things,
+        setThings = Hook.useState [| "Raindrops on roses"
+                                     "Whiskers on kittens"
+                                     "Bright copper kettles"
+                                     "Warm woolen mittens" |]
+        
+        let private deleteThing(index: int) =
+            things |> Array.
