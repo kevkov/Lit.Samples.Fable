7fc043ef3acec5866b3de8eca4e63945e25ad628
kevkovh <22GNewReggy@^>
2022-05-20 18:41:55 +0100

    reactivity 3

---
+++ src/Reactivity/Three.fs
@@ -0,0 +1,38 @@
+﻿namespace Lit.Samples.Fable.Reactivity
+
+open Fable.Core.JS
+
+module Three =
+
+    open Lit
+
+    type List<'t> with
+        static member filteri f (l: List<'t>) = l |> List.indexed |> List.filter f
+
+    [<LitElement("my-element")>]
+    let MyElement () =
+        let _, props =
+            LitElement.init (fun init ->
+                init.props <-
+                    {| groceries =
+                        Prop.Of [ "tea"
+                                  "milk"
+                                  "honey"
+                                  "chocolate" ] |})
+
+        let removeItem (item: string) =
+            let indexToRemove =
+                props.groceries.Value
+                |> List.findIndex (fun g -> g = item)
+
+            printfn $"{indexToRemove}"
+
+            props.groceries.Value <-
+                props.groceries.Value
+                |> List.filteri (fun (i, g) -> i <> indexToRemove)
+                |> List.map snd
+
+        html
+            $"""
+              {props.groceries.Value
+               |> List.map (fun item -> html $"<button @click={fun _ -> removeItem (item)}>x</button>  ${item}<br>")}"""
