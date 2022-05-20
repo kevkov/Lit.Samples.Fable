b0399d8df3e5937f43ae0bb7bc033b98c7b2a642
kevkovh <22GNewReggy@^>
2022-05-20 17:45:35 +0100

    reactivity 1,2

---
+++ src/Reactivity/OneAndTwo.fs
@@ -0,0 +1,24 @@
+﻿namespace Lit.Samples.Fable.Reactivity
+
+open System
+
+module One =
+
+    open Lit
+
+    [<LitElement("my-element")>]
+    let MyElement () =
+        let _, props =
+            LitElement.init (fun init -> init.props <- {| result = Prop.Of(defaultValue = "") |})
+
+        let flipCoin _ =
+            if Random().NextDouble () < 0.5 then
+                props.result.Value <- "Heads"
+            else
+                props.result.Value <- "Tails"
+
+        html
+            $"""
+            <button @click={flipCoin}>Flip a coin!</button>
+            <p>Result: {props.result}</p>
+            """
